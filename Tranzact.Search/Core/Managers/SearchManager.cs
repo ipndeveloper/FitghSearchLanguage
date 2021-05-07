556332a1c2647656844a11e6d02f754d689ebeee
ipndeveloper <i_nav_31_8@hotmail.com>
2021-05-06 22:06:07 -0500

    d

--- Tranzact.Search/Core/Managers/SearchManager.cs
+++ Tranzact.Search/Core/Managers/SearchManager.cs
@@ -50,42 +50,6 @@ namespace Tranzact.Search
         }
 
     }
-    //public class SearchManager : ISearchManager<SearchResponse>
-    //{
-
-    //    protected internal IConfigurationRoot  Configuration { get; set; }
-    //    protected internal IEnumerable<ISearch<SearchResponse>> Search { get; set; }
-    //    protected  internal IList<Task<SearchResponse>> SearchResponse { get; } = new List<Task<SearchResponse>>();
-    //    public SearchManager(IEnumerable<ISearch<SearchResponse>> searchs ,
-    //        IConfigurationRoot configuration)
-    //    {
-    //        Search = searchs;
-    //        Configuration = configuration;
-    //    }
-    //    public virtual async Task<List<SearchResponse>> GetSearchResultsAsync(string[] searchQuery)
-    //    {
-    //        string[] words = Utility.FixSearchQuery(searchQuery);
-    //        IList<Task<SearchResponse>> allSearchTask = this.GetAllResponseSearch(words);
-    //        var response =await Task.WhenAll(allSearchTask);
-    //        return response.ToList();
-    //    }
-    //    private IList<Task<SearchResponse>> GetAllResponseSearch(string[] searchQuery)
-    //    {
-    //        foreach (var word in searchQuery)
-    //        {
-    //            foreach (var search  in Search)
-    //            {
-    //                SearchResponse.Add(search.GetSearchInfoAsync(word));
-    //            }
-    //        }
-    //        return SearchResponse;
-    //    }
-
-    //    public void Dispose()
-    //    {
-
-    //    }
-    //}
-
+    
 
 }
